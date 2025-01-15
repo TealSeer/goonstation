407477c836672b0444f507fb91a74cf8bba1c6e6
Roxy <75404941+TealSeer@users.noreply.github.com>
2025-01-15 17:15:03 -0500

    b

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 source buildByond.conf
 
+original_dir=$PWD
+cd "$1"
 wget -O ./librust_g.so "https://github.com/goonstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
 chmod +x ./librust_g.so
 ldd ./librust_g.so
+cd "$original_dir"
